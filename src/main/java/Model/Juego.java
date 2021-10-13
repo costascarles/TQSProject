46a1fb291f515036e0532457d9b211fd9d57e2cc
Carles Costas <carles_cosmat@hotmail.es>
2021-10-13 11:11:29 +0200

    Functions to modify and calculate the matrix positions Functions implemented setMatrixValue() and printarMarca()

--- src/main/java/Model/Juego.java
+++ src/main/java/Model/Juego.java
@@ -37,12 +37,49 @@ public class Juego {
     public void turno(int pos){
 
     }
-    public void pintarMarca(int pos,String marca){
-
-
+    public boolean pintarMarca(int pos,String marca){
+        boolean result=false;
+        switch (pos){
+            case 1:
+                result = setMatrixValue(0,0,marca);
+                break;
+            case 2:
+                result = setMatrixValue(0,1,marca);
+                break;
+            case 3:
+                result = setMatrixValue(0,2,marca);
+                break;
+            case 4:
+                result = setMatrixValue(1,0,marca);
+                break;
+            case 5:
+                result = setMatrixValue(1,1,marca);
+                break;
+            case 6:
+                result = setMatrixValue(1,2,marca);
+                break;
+            case 7:
+                result = setMatrixValue(2,0,marca);
+                break;
+            case 8:
+                result = setMatrixValue(2,1,marca);
+                break;
+            case 9:
+                result = setMatrixValue(2,2,marca);
+                break;
+        }
+        return result;
     }
-    public void setMatrixValue(int x,int y,String marca){
+    public boolean setMatrixValue(int x,int y,String marca){
+        if (x<3 && x>-1 && y<3 && y>-1){
+            if(tablero[x][y] == null){
+                tablero[x][y]=marca;
+                return true;
+            }
+
+        }
 
 
+        return false;
     }
 }
